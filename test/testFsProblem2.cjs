f6eccfcf47f7d519ef7de4e8fa7b2c833d81d63c
PavaniNerusu37 <nerusupavani3737@gmail.com>
2024-01-10 11:58:32 +0530

    commited

--- test/testFsProblem2.cjs
+++ test/testFsProblem2.cjs
@@ -1,5 +1,5 @@
 //importing function from problem.cjs file
-const problem2 = require("../problem2.cjs");
+const problem2 = require("../fs-problem2.cjs");
 
 //calling the imported funtion with test file path as argument
 problem2("lipsum.txt");
